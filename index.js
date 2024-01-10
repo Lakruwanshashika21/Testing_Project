fad4a95f3bd3101bbd5782451824d25e32667805
LSP_Devoloper <135962682+Lakruwanshashika21@users.noreply.github.com>
2024-01-10 20:51:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.getElementById("myButton").onclick = function(){
     username1 = document.getElementById("mytext").value;
     document
     console.log(username1);
-    document.getElementById("myName").innerHTML = "Hello " +  username1 + "........";
+    document.getElementById("myName").innerHTML = "Hello " +  username1 + " I Love you so much";
     //document.location='final.html';
 }
 
