ebca282b5948569bf3b8bfb58e77dd99bb3600a7
LSP_Devoloper <135962682+Lakruwanshashika21@users.noreply.github.com>
2024-01-10 20:18:07 +0530

    Add files via upload

---
+++ Testing Project/index.js
@@ -0,0 +1,11 @@
+let username1;
+
+document.getElementById("myButton").onclick = function(){
+
+    username1 = document.getElementById("mytext").value;
+    document
+    console.log(username1);
+    document.getElementById("myName").innerHTML = "Hello " +  username1 + "........";
+    //document.location='final.html';
+}
+
